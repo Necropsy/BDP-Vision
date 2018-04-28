8e56a93ad98cbed730adf3fb03c5acee45321013
Elcio Jr <elciojuniorponto@gmail.com>
2018-04-27 23:26:44 -0300

    ADD - Pacote no padrão BDP

---
+++ build/CMakeFiles/vision.dir/DependInfo.cmake
@@ -0,0 +1,99 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/necropsy/ssl-vision/build/qrc_icons.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/qrc_icons.cxx.o"
+  "/home/necropsy/ssl-vision/build/qrc_icons_gui.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/qrc_icons_gui.cxx.o"
+  "/home/necropsy/ssl-vision/src/app/capture_thread.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/capture_thread.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/framedata.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/framedata.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/gui/cameracalibwidget.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/cameracalibwidget.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/gui/colorpicker.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/colorpicker.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/gui/glLUTwidget.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/glLUTwidget.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/gui/glwidget.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/glwidget.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/gui/jog_dial.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/jog_dial.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/gui/lutwidget.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/lutwidget.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/gui/mainwindow.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/mainwindow.cpp.o"
+  "/home/necropsy/ssl-vision/build/src/app/gui/moc_cameracalibwidget.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/moc_cameracalibwidget.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/gui/moc_glLUTwidget.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/moc_glLUTwidget.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/gui/moc_glwidget.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/moc_glwidget.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/gui/moc_jog_dial.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/moc_jog_dial.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/gui/moc_lutwidget.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/moc_lutwidget.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/gui/moc_mainwindow.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/moc_mainwindow.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/gui/moc_videowidget.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/moc_videowidget.cxx.o"
+  "/home/necropsy/ssl-vision/src/app/gui/realtimedisplaywidget.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/realtimedisplaywidget.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/gui/renderoptions.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/renderoptions.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/gui/videowidget.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/gui/videowidget.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/main.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/main.cpp.o"
+  "/home/necropsy/ssl-vision/build/src/app/moc_capture_thread.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/moc_capture_thread.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/plugins/moc_plugin_colorcalib.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/moc_plugin_colorcalib.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/plugins/moc_plugin_dvr.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/moc_plugin_dvr.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/plugins/moc_plugin_legacypublishgeometry.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/moc_plugin_legacypublishgeometry.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/plugins/moc_plugin_publishgeometry.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/moc_plugin_publishgeometry.cxx.o"
+  "/home/necropsy/ssl-vision/build/src/app/plugins/moc_visionplugin.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/moc_visionplugin.cxx.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_cameracalib.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_cameracalib.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_colorcalib.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_colorcalib.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_colorthreshold.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_colorthreshold.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_detect_balls.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_detect_balls.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_detect_robots.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_detect_robots.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_dvr.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_dvr.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_find_blobs.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_find_blobs.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_legacypublishgeometry.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_legacypublishgeometry.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_legacysslnetworkoutput.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_legacysslnetworkoutput.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_publishgeometry.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_publishgeometry.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_runlength_encode.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_runlength_encode.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_sslnetworkoutput.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_sslnetworkoutput.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/plugin_visualize.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/plugin_visualize.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/plugins/visionplugin.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/plugins/visionplugin.cpp.o"
+  "/home/necropsy/ssl-vision/build/src/app/stacks/moc_multistack_robocup_ssl.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/stacks/moc_multistack_robocup_ssl.cxx.o"
+  "/home/necropsy/ssl-vision/src/app/stacks/multistack_robocup_ssl.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/stacks/multistack_robocup_ssl.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/stacks/multivisionstack.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/stacks/multivisionstack.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/stacks/stack_robocup_ssl.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/stacks/stack_robocup_ssl.cpp.o"
+  "/home/necropsy/ssl-vision/src/app/stacks/visionstack.cpp" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/app/stacks/visionstack.cpp.o"
+  "/home/necropsy/ssl-vision/build/src/shared/util/moc_lut3d.cxx" "/home/necropsy/ssl-vision/build/CMakeFiles/vision.dir/src/shared/util/moc_lut3d.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtOpenGL"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtNetwork"
+  "/usr/include/qt4/QtCore"
+  "."
+  "/usr/include/eigen3"
+  "../src/shared/capture"
+  "../src/shared/cmvision"
+  "../src/shared/cmpattern"
+  "../src/shared/gl"
+  "../src/shared/net"
+  "../src/shared/util"
+  "../src/shared/vartypes"
+  "../src/shared/vartypes/primitives"
+  "../src/shared/vartypes/enhanced"
+  "../src/shared/vartypes/gui"
+  "../src/shared/vartypes/xml"
+  "../src/app"
+  "../src/app/gui"
+  "../src/app/plugins"
+  "../src/app/stacks"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/necropsy/ssl-vision/build/CMakeFiles/sslvision.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/clientBDP/main.cpp
+++ src/clientBDP/main.cpp
@@ -16,8 +16,6 @@
 #include <netdb.h>
 #include <string.h>
 
-#include <string>
-
 //Parametros para o servidor
 #define BDP_Tbuffer 1024
 #define PORTA 13822
@@ -25,20 +23,22 @@
 //===========================================================
 
 //Valor ficticio
-#define NA 999.0
+#define NA 9999.0
 //Tamanho do vetor BDP_MSG
-#define MSG 33
+#define MSG 27
 
 using namespace std;
 
 //Globais:
-//BDP_MSG gasta ou todo 33 posições
 float BDP_MSG[MSG];
-char BDP[100];
+char BDP[115];
 int varControle = 0;
 
 //Padrão de msg BDP:
-//BOLA:[conf.][posX][posY] TIME1:[id][conf.][posX][posY][ang.] TIME2:[id][conf.][posX][posY][ang]
+//OBS: Tem que desativar a opção d epublicar a geometria no ssl-vision
+//BOLA:[conf.][posX][posY] TIME1:[id][posX][posY][ang.] TIME2:[id][posX][posY][ang]
+//B_C  B_X  B_Y |R_I  R_X  R_Y  R_A |R_I  R_X  R_Y  R_A |R_I  R_X  R_Y  R_A |R_I  R_X  R_Y  R_A |R_I  R_X  R_Y  R_A |R_I  R_X  R_Y  R_A
+//9999 9999 9999|0000 0000 0000 0000|0000 0000 0000 0000|0000 0000 0000 0000|0000 0000 0000 0000|0000 0000 0000 0000|0000 0000 0000 0000
 
 //Funções:
 void RobotInfo(const SSL_DetectionRobot & robot) {
@@ -48,30 +48,34 @@ void RobotInfo(const SSL_DetectionRobot & robot) {
     } else {
         BDP_MSG[varControle] = NA;
     }
-    BDP_MSG[varControle+1] = robot.confidence();
-    BDP_MSG[varControle+2] = robot.x();
-    BDP_MSG[varControle+3] = robot.y();
+    BDP_MSG[varControle+1] = robot.x();
+    BDP_MSG[varControle+2] = robot.y();
     if (robot.has_orientation()) {
-        BDP_MSG[varControle+4] = robot.orientation();
+        BDP_MSG[varControle+3] = (robot.orientation()*100);
     } else {
-        BDP_MSG[varControle+4] = NA;
+        BDP_MSG[varControle+3] = NA;
     }
-
-    varControle += varControle+5;
+    varControle = varControle+4;
 }
 
 void toVecChar(){
-	char auxMSG[5];
-	BDP[0] = '&';
-	for(int i=0; i<MSG; i++){
-		printf("%d ", int(BDP_MSG[i]));
-	}
 	int j;
+
+	//inicializo todos os vetores relacionados a mensagem,
+	//e que serão usados para a criação do padrão BDP, foi
+	//usada a alocação dinâmica para não estourar a memória
+	//da máquina
+	char *auxMSG;
+	char *BDP2;
+	BDP2 = (char *) malloc(115*sizeof(char));
 	for(j=0; j<MSG; j++){
-		sprintf(auxMSG, "%d", int(BDP_MSG[j]));
-		strcat(BDP,auxMSG);
+		auxMSG = (char *) malloc(12*sizeof(char));
+		sprintf(auxMSG, "%04d", int(BDP_MSG[j]));
+		strcat(BDP2,auxMSG);
+		free(auxMSG);
 	}
-	BDP[j] = '$';
+	strcpy(BDP, BDP2);
+	free(BDP2);
 }
 
 int main(){
@@ -81,29 +85,24 @@ int main(){
 	//Neste caso, o cliente ja esta por default
 	//conectado no ssl, não é necessario informar
 	//o IP e a PORTA (224.5.23.2 , 10006)
-    
+
     client.open(true);
-    
+
     //Instanciando o pacote (obtido dos arq. protobuf)
     SSL_WrapperPacket packet;
 
     //=======================SERVIDOR=======================
     struct sockaddr_in BDP_Servidor;
     int BDP_Socket;
-    char BDP_buffer[BDP_Tbuffer];
 
     BDP_Socket = socket(PF_INET, SOCK_DGRAM, 0);
     BDP_Servidor.sin_family = AF_INET;
     BDP_Servidor.sin_port = htons(PORTA);
-    BDP_Servidor.sin_addr.s_addr = inet_addr("192.168.0.171");
-
-    memset(BDP_buffer, '\0', BDP_Tbuffer);
-
-    //bind(meuSocket, (struct sockaddr*)&servidor, sizeof(sockaddr_in));
+    BDP_Servidor.sin_addr.s_addr = htonl(INADDR_ANY);
     //======================================================
 
     while(true){
-    	
+
     	//verifica se algum pacote foi recebido
     	if(client.receive(packet)){
 
@@ -118,7 +117,7 @@ int main(){
     			int balls_n = detection.balls_size();
     			if(balls_n >= 1){
     				SSL_DetectionBall ball = detection.balls(0);
-    				BDP_MSG[varControle] = ball.confidence();
+    				BDP_MSG[varControle] = (ball.confidence())*100;
     				BDP_MSG[varControle+1] = ball.x();
     				BDP_MSG[varControle+2] = ball.y();
     			}else{
@@ -133,20 +132,17 @@ int main(){
     			varControle += varControle+3;
 
     			//TIME1:
-                int robots_blue_n = detection.robots_blue_size();
-    			
-    			//***Limitando a 3 robos por time (EM ANALISE SE É
-    			//***VIÁVEL LIMITAR)
-    			//***if(robots_blue_n>3){robots_blue_n = 3;}
-    			
+          		int robots_blue_n = detection.robots_blue_size();
+
+    			//***Limitando a 3 robos por time
     			for(int i=0; i<robots_blue_n; i++){
     				SSL_DetectionRobot robot = detection.robots_blue(i);
     				RobotInfo(robot);
     			}
 
     			//TIME2:
-                int robots_yellow_n = detection.robots_yellow_size();
-    			
+          		int robots_yellow_n = detection.robots_yellow_size();
+
     			for(int i=0; i<robots_yellow_n; i++){
     				SSL_DetectionRobot robot = detection.robots_yellow(i);
     				RobotInfo(robot);
@@ -157,27 +153,13 @@ int main(){
     			//seta o BDP_MSG com a flag -1 no inicio, esta implementação
     			//foi pensada para calcular futuramente a quantidade de falhas
     			//na obtenção do pacote diretamente do SSL-Vision
-    			BDP_MSG[varControle] = -1;
+    			BDP_MSG[varControle] = -NA;
     		}
 
-    		//Print da mensagem criada para debug
-    		//printf("======================== Novo Pacote ========================\n");
-    		//for(int i=0; i<MSG; i++){
-    		//	printf("%f ", BDP_MSG[i]);
-    		//}
-
-    		//Envia um pacote no IP e PORTA especificado============================
-    		memset(BDP, '\0', 100);
+    		//Envia um pacote no IP ou PORTA especificado===========================
     		toVecChar();
-    		int BDP_Envio = sendto(BDP_Socket, BDP, sizeof(BDP), 0, (struct sockaddr*)&BDP_Servidor, sizeof(sockaddr_in));
-    		if(BDP_Envio){
-    			printf("=======================================Pacote Enviado!=======================================\n");
-    		}else{
-    			printf("=======================================Falha no Envio!=======================================\n");
-    		}
-    		memset(BDP_buffer, '\0', BDP_Tbuffer);
+    		sendto(BDP_Socket, BDP, sizeof(BDP), 0, (struct sockaddr*)&BDP_Servidor, sizeof(sockaddr_in));
     		//======================================================================
-
     	}
     	//Reseta a variavel de controle para o proximo
     	//pacote que iremos receber
